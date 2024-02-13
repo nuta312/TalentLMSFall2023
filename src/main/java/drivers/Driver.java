43271570797fdbc82ba16632769819466074c0c8
Ruskano <ruskano1991@gmail.com>
2024-02-13 17:45:04 +0600

    TL-005/Scrum-8/feature - added classes: ChromeWebDriver, EdgeWebDriver, FireFoxDriver Driver and added three file .exe

---
+++ src/main/java/drivers/Driver.java
@@ -0,0 +1,45 @@
+package drivers;
+
+import config.ConfigReader;
+import org.openqa.selenium.WebDriver;
+
+public class Driver {
+    private Driver() {
+        //Singleton
+    }
+    private static WebDriver driver; //объявляем driver для управления браузером
+
+    /**
+     * создаем кастомный метод getDriver для переключения между браузерами указанными в app.properties
+     */
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            switch (ConfigReader.getValue("browser").toLowerCase()) {
+                case "chrome":
+                    driver = ChromeWebDriver.loadChromeDriver();
+                    break;
+                case "edge":
+                    driver = EdgeWebDriver.loadEdgeDriver();
+                    break;
+                case "firefox":
+                    driver = FireFoxWebDriver.loadFirefoxDriver();
+                    break;
+                //TODO add macOS
+                default:
+                    throw new IllegalArgumentException("You provided wrong Driver name");
+            }
+        } return driver;
+    }
+    public static void closeDriver(){ // нужен по умолчанию
+        try {
+            if (driver!=null){
+                driver.close();
+                driver.quit();
+                driver=null;
+            }
+        }catch (Exception e){
+            System.out.println("Error while closing driver");
+        }
+    }
+
+}
---
+++ src/main/java/drivers/EdgeWebDriver.java
@@ -0,0 +1,16 @@
+package drivers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
+public class EdgeWebDriver {
+    public static WebDriver loadEdgeDriver(){
+        System.setProperty("webdriver.edge.driver","src/main/resources/drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+        driver.manage().window().maximize();
+        return driver;
+    }
+}
