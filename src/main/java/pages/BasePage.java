124e766e34cce73275ec51a22ca426d6d82bbbe6
Your Name <you@example.com>
2024-02-14 23:46:57 +0600

    fixed/ TL-002

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,11 +1,46 @@
 package pages;
 
+import driver.Driver;
+import helper.AlertHelper;
+import helper.BrowserManager;
+import helper.DropDownHelper;
+import helper.FrameHelper;
+import helper.WebElementHelper;
+import helper.WindowHelper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
-public class BasePage {
+public abstract class BasePage {
+
+    WebDriver webDriver;
+    AlertHelper alertHelper = new AlertHelper();
+    BrowserManager browserManager = new BrowserManager(webDriver);
+    DropDownHelper dropDownHelper = new DropDownHelper();
+    FrameHelper frameHelper = new FrameHelper(webDriver);
+    WebElementHelper webElementHelper = new WebElementHelper();
+    WindowHelper windowHelper = new WindowHelper();
+
+    @BeforeClass
+    public void seTup(){
+        webDriver = Driver.getDriver();
+        alertHelper = new AlertHelper();
+        browserManager = new BrowserManager(webDriver);
+        dropDownHelper = new DropDownHelper();
+        frameHelper = new FrameHelper(webDriver);
+        webElementHelper = new WebElementHelper();
+        windowHelper = new WindowHelper();
+    }
+
+    @AfterClass
+    public void closeRecourses(){
+        webDriver.close();
+        webDriver.quit();
+    }
 
     public BasePage(WebDriver driver) {
+
         PageFactory.initElements(driver,this);
     }
 }
