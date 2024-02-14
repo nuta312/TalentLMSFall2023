bc917348e2f4a5ecd312e81954f43801eb2cbee7
Janar <zhakubai@gmail.com>
2024-02-14 21:38:12 +0600

    features/ TL-016 added URL Enum

---
+++ src/main/java/helper/AlertHelper.java
@@ -0,0 +1,47 @@
+package helper;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class AlertHelper {
+
+        public Alert getAlert() {
+            return Driver.getDriver().switchTo().alert();
+        }
+        public void acceptAlert() {
+
+            new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(7))
+                    .until(ExpectedConditions.alertIsPresent());
+        if (!isAlertPresent()){
+            return;
+        }
+            getAlert().accept();
+
+        }
+        public void dismissAlert() {
+            new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(7))
+                    .until(ExpectedConditions.alertIsPresent());
+            if (!isAlertPresent()){
+                return;
+            }
+            getAlert().dismiss();
+        }
+        public void sendKeysAlert(String txt) {
+            if (!isAlertPresent()){
+                return;
+            }
+            getAlert().sendKeys(txt);
+        }
+
+        public boolean isAlertPresent(){
+            try {
+                Driver.getDriver().switchTo().alert();
+                return true;
+            }catch (NoAlertPresentException e){
+                return false;
+            }
+        }
+    }
+
---
+++ src/main/java/helper/URL.java
@@ -0,0 +1,9 @@
+package helper;
+
+public enum URL {
+
+        TALENTLMS("https://www.talentlms.com/");
+
+        URL(String url) {
+        }
+    }
