e68e1f38bdcf0b4bf93c21f69ff5a9bc0f080d9f
HP <asanbeekovb@gmail.com>
2024-02-15 10:18:40 +0600

    "TL-020: Domain"

---
+++ src/test/java/uiTest/DomainTest.java
@@ -0,0 +1,32 @@
+package uiTest;
+
+import driver.Driver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import pages.DomainPage;
+
+public class DomainTest {
+    WebDriver driver;
+
+    public DomainTest() {
+        this.driver = Driver.getDriver();
+    }
+
+    @Test
+    void Domain() throws InterruptedException {
+        driver.get("https://fall2023.talentlms.com/index");
+        DomainPage domainPage = new DomainPage();
+        domainPage.AzhanLogin()
+                .PathDomain()
+                 .DomainName()
+                  .MapACustomDomain()
+                   .PopupWindow()
+                    .UpgradeClick()
+                     .Rename()
+                      .CancelClick()
+                       .ClickHome()
+                        .ChangeName()
+                         .ReturnToDomain();
+        driver.quit();
+    }
+}
