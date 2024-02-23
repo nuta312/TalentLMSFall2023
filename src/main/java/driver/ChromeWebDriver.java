98b3b226b831bc5bcd31bdb6aa8845f81f5f6646
nuta312 <musakunovnur@gmail.com>
2024-02-23 11:51:14 +0600

    Update ChromeWebDriver.java

--- src/main/java/driver/ChromeWebDriver.java
+++ src/main/java/driver/ChromeWebDriver.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static config.ConfigReader.getValue;
 
